6f697edb80cfada6c09f6fef124e3119899816fb
akansal307 <angelkansal30@gmail.com>
2026-09-05 01:15:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SIH-2026/',
+  base: '/',
 })
